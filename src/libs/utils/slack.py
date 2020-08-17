c1d7a1169e7e84b4e988815bfb4f2330cc38d05c
jh.park <jh.park@ridi.com>
2020-08-17 13:11:44 +0900

    슬랙 메세지 보낼때 key 가 없으면 보내지 않도록 함

--- src/libs/utils/slack.py
+++ src/libs/utils/slack.py
@@ -5,6 +5,9 @@ from slacker import Slacker
 
 
 def slack_notify(channel: str, text: str = None, username: str = 'ghost', attachments: List[Dict] = None):
+    if not settings.SLACK_API_KEY:
+        return
+
     Slacker(settings.SLACK_API_KEY).chat.post_message(
         channel=channel, text=text, username=username, attachments=attachments, icon_emoji=':ghost:'
     )
